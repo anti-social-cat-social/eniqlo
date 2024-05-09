4c685bc15f839d6b7b5ce42968e5c17b3f127fc0
Lathif Al Rasyid <lathif.rasyid-guest@moladin.com>
2024-05-09 22:33:55 +0700

    add endpoint staff register and login

--- server/routes.go
+++ server/routes.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"eniqlo/internal/staff"
+	"eniqlo/pkg/response"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,11 +11,28 @@ import (
 
 func NewRoute(engine *gin.Engine, db *sqlx.DB) {
 	// Handle for not found routes
+	engine.NoRoute(NoRouteHandler)
 	router := engine.Group("v1")
 
 	router.GET("ping", pingHandler)
+
+	initializeStaffHandler(db, router)
 }
 
+func initializeStaffHandler(db *sqlx.DB, router *gin.RouterGroup) {
+	// Initialize all necessary dependecies
+	staffRepo := staff.NewStaffRepository(db)
+	staffUc := staff.NewStaffUsecase(staffRepo)
+	staffH := staff.NewStaffHandler(staffUc)
+
+	staffH.Router(router)
+}
+
+func NoRouteHandler(ctx *gin.Context) {
+	response.GenerateResponse(ctx, http.StatusNotFound, response.WithMessage("Page not found"))
+}
+
+// Handler for ping request from routes
 func pingHandler(ctx *gin.Context) {
 	ctx.JSON(
 		http.StatusOK,
